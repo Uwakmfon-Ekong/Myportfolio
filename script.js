2e877b4d718dc4d124057f10bdf194e34bddb30d
Uwakmfon-Ekong <uwakmfone470@gmail.com>
2023-09-12 12:33:03 +0100

    update made

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multipletext", {
+  strings: ["frontend developer", "digital marketer", "social media marketer"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backdelay: 1000,
+  loop: true,
+});
